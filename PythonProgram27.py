0059df88487f86d3c3861aa99b76dd34a8837aa6
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-29 11:20:32 +0600

    Create PythonProgram27.py

---
+++ PythonProgram27.py
@@ -0,0 +1,13 @@
+# list part 1
+subjects = ['python', 'java', 'car']
+print(subjects)
+print(subjects[0])
+print(subjects[1:])
+print(subjects[-1])
+# in / not in
+print("java" in subjects)
+print("ada" in subjects)
+print("ada" not in subjects)
+# add  anything
+print(subjects + [' rubi ' ,89] )
+print(subjects * 3)
